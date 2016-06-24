ccead463c358519595a865b0b5da022e24562b21
jo-im <joann_im@hotmail.com>
2016-06-23 19:17:50 -0700

    Added url, initialize, parse to fetch song data
    using this.fetch to initialize songs

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -3,4 +3,30 @@ var Songs = Backbone.Collection.extend({
 
   model: SongModel,
 
+  url: 'https://api.parse.com/1/classes/songs/',
+
+  initialize: function() {
+    this.fetch();  
+  },
+
+  parse: function(response) {
+    // console.log('going to push to songData');
+    // console.log('songData', songData);
+    return response.results;
+  }
+
+  // sync: function(method, model, options) {
+  //   var that = this;
+  //   var params = _.extend({
+  //     type: 'GET',
+  //     dataType: 'json',
+  //     url: that.url,
+  //     contentType: 'application/json',
+  //     processData: false
+  //   }, options);
+
+  //   console.log('params', params);
+  //   return $.ajax(params);
+  // }
+
 });
